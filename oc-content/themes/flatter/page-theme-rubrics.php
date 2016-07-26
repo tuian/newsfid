d3553d5e2dce5a97d93eb6dc654cf665425b1f30
jaysukh <jaysukh.maghodiya@gmail.com>
2016-07-26 19:16:28 +0530

    implement themes and rubrics and soundpass page

--- oc-content/themes/flatter/page-theme-rubrics.php
+++ oc-content/themes/flatter/page-theme-rubrics.php
@@ -1,120 +1,179 @@
 <?php
-// meta tag robots
-error_reporting(0);
 osc_add_hook('header', 'flatter_nofollow_construct');
-
 flatter_add_body_class('register');
 osc_enqueue_script('jquery-validate');
 osc_current_web_theme_path('header.php');
 
-
 $data = new DAO();
-$data->dao->select("a.*, b.*, c.i_num_items, d.*");
-$data->dao->from(DB_TABLE_PREFIX . 't_category as a');
-$data->dao->join(DB_TABLE_PREFIX . 't_category_description as b', 'a.pk_i_id = b.fk_i_category_id', 'INNER');
-$data->dao->join(DB_TABLE_PREFIX . 't_category_stats  as c ', 'a.pk_i_id = c.fk_i_category_id', 'LEFT');
-$data->dao->join(DB_TABLE_PREFIX . 'bs_theme_category_icon  as d ', 'a.pk_i_id = d.pk_i_id', 'LEFT');
-//$data->dao->where(sprintf("a.fk_i_parent_id in (%s)", $_SESSION['theme_ids']));
-//$data->dao->whereIn("a.fk_i_parent_id", $_SESSION['theme_ids']);
-$data->dao->where("b.fk_c_locale_code = 'en_US'");
-$data->dao->orderBy('a.pk_i_id', 'ASC');
-$data->dao->limit('10');
-$result1 = $data->dao->get();
+
+$user_id = osc_logged_user_id();
+$data = new DAO();
+$data->dao->select("theme.theme_id");
+$data->dao->from(DB_TABLE_PREFIX . 't_user_themes as theme');
+$data->dao->where("theme.user_id = $user_id");
+$theme_list = $data->dao->get();
+$selected_themes = $theme_list->result();
+$selected_themes_arr = array();
+foreach ($selected_themes as $sl):
+    $selected_themes_arr[] = $sl['theme_id'];
+endforeach;
+
+$data1 = new DAO();
+$data1->dao->select("a.*, b.*, d.*");
+$data1->dao->from(DB_TABLE_PREFIX . 't_category as a');
+$data1->dao->join(DB_TABLE_PREFIX . 't_category_description as b', 'a.pk_i_id = b.fk_i_category_id', 'INNER');
+//$data1->dao->join(DB_TABLE_PREFIX . 't_category_stats  as c ', 'a.pk_i_id = c.fk_i_category_id', 'LEFT');
+$data1->dao->join(DB_TABLE_PREFIX . 'bs_theme_category_icon  as d ', 'a.pk_i_id = d.pk_i_id', 'LEFT');
+$data1->dao->where("a.fk_i_parent_id IS NULL AND b.fk_c_locale_code = 'en_US'");
+$data1->dao->orderBy('a.pk_i_id', 'ASC');
+$result1 = $data1->dao->get();
 $themes = $result1->result();
+$active = '1';
+if (isset($_GET['type']) && $_GET['type'] == 'interest'):
+    $active = '2';
+    $intrest_data = new DAO();
+    $intrest_data->dao->select("a.*, b.*, d.*");
+    $intrest_data->dao->from(DB_TABLE_PREFIX . 't_category as a');
+    $intrest_data->dao->join(DB_TABLE_PREFIX . 't_category_description as b', 'a.pk_i_id = b.fk_i_category_id', 'INNER');
+    //$intrest_data->dao->join(DB_TABLE_PREFIX . 't_category_stats  as c ', 'a.pk_i_id = c.fk_i_category_id', 'LEFT');
+    $intrest_data->dao->join(DB_TABLE_PREFIX . 'bs_theme_category_icon  as d ', 'a.pk_i_id = d.pk_i_id', 'LEFT');
+    $intrest_data->dao->where(sprintf("a.fk_i_parent_id in (%s) AND b.fk_c_locale_code = 'en_US'", implode(",", $selected_themes_arr)));
+    $intrest_data->dao->orderBy('a.pk_i_id', 'ASC');
+    $result = $intrest_data->dao->get();
+    $interest = $result->result();
+   
+    $rdata = new DAO();
+    $rdata->dao->select("rubric.rubric_id");
+    $rdata->dao->from(DB_TABLE_PREFIX . 't_user_rubrics as rubric');
+    $rdata->dao->where("rubric.user_id = $user_id");
+    $theme_list = $rdata->dao->get();
+    $selected_rubric = $theme_list->result();
+    $selected_rubric_arr = array();
+    foreach ($selected_rubric as $sl):
+        $selected_rubric_arr[] = $sl['rubric_id'];
+    endforeach;
+endif;
 
 
-if ($_REQUEST['submit']):
-    if (count($_REQUEST['cat_id']) >= 4):
-        $user_id = $_SESSION['user_id'];
-        $conn = getConnection();
-        foreach ($_REQUEST['cat_id'] as $k => $v):
-            $conn->osc_dbExec("INSERT INTO %st_user_rubrics ( user_id, rubric_id) VALUES (%s,'%s' )", DB_TABLE_PREFIX, $user_id, $v);
-        endforeach;
-        osc_reset_static_pages();
 
-        $user = User::newInstance()->findByPrimaryKey($user_id);
-        Session::newInstance()->_set('userId', $user['pk_i_id']);
-        Session::newInstance()->_set('userName', $user['s_name']);
-        Session::newInstance()->_set('userEmail', $user['s_email']);
-        $phone = ($user['s_phone_mobile']) ? $user['s_phone_mobile'] : $user['s_phone_land'];
-        Session::newInstance()->_set('userPhone', $phone);
-        header('Location: ' . osc_base_url());
-        die;
+if ($_POST):
+    $conn = getConnection();
+    if (isset($_GET['type']) && $_GET['type'] == 'interest'):
+        if (count($_REQUEST['cat_id']) >= 4):
+            $conn->osc_dbExec("DELETE FROM `%st_user_rubrics` WHERE `user_id` = %s", DB_TABLE_PREFIX, $user_id);
+            foreach ($_REQUEST['cat_id'] as $k => $v):
+                $conn->osc_dbExec("INSERT INTO %st_user_rubrics ( user_id, rubric_id) VALUES (%s,'%s' )", DB_TABLE_PREFIX, $user_id, $v);
+            endforeach;           
+            osc_add_flash_info_message(_m('Rubriques updated successfully'));
+            header("Location: " . $_SERVER['REQUEST_URI']);
+        else:
+            osc_add_flash_error_message(_m('You must select at least four rubrics'));
+            header("Location: " . $_SERVER['REQUEST_URI']);
+        endif;
     else:
-        osc_add_flash_error_message(_m('You must select at least four rubrics'));
-        header("Location: " . $_SERVER['REQUEST_URI']);
+        if (count($_REQUEST['cat_id']) >= 1):
+            $conn->osc_dbExec("DELETE FROM `%st_user_themes` WHERE `user_id` = %s", DB_TABLE_PREFIX, $user_id);
+            foreach ($_REQUEST['cat_id'] as $k => $v):
+                $conn->osc_dbExec("INSERT INTO %st_user_themes ( user_id, theme_id) VALUES (%s,'%s' )", DB_TABLE_PREFIX, $user_id, $v);
+            endforeach;
+            osc_add_flash_info_message(_m('Theme updated successfully'));
+            header('Location: ' . osc_static_page_url());
+        else:
+            osc_add_flash_error_message(_m('You must select at least one theme'));
+            header("Location: " . $_SERVER['REQUEST_URI']);
+        endif;
     endif;
 endif;
+
 ?>
-<style>
-    .user-interest h3{
-        padding: 0px 20px;
-        font-weight: bold;
-        color: #000;
-    }   
-    .user-interest .theme-interest-tab{
-        display: inline-block;        
-        padding: 0;
-        font-weight: bold;
-        color: #000;
-        cursor: pointer;
-        font-size: 16px;
-        margin-left: 40px;
-    }
-    .user-interest .theme-interest-tab.active{        
-        border-bottom: 2px solid #000;  
-        color: #FF9900!important;
 
-    }
-    .user-interest .user_rubric_form .col-md-2{
-        width: 20%;
-    }
-
-</style>
 <div id="columns" class="user-interest">
-    <h3>Centre d'intérêt</h3>
+    <h3 class="col-md-9">Centre d'intérêt</h3>
+    <div class="col-md-3 pull-right">
+        <input class="btn btn-blue btn-flat update_theme" type="submit" name="submit" value="Enregistrer" />
+    </div>
+    <div class="clearfix"></div>
     <div class="theme-interest-container">
         <ul class="nav">
-            <li class="theme-interest-tab active" data-id="theme">Themes</li>
-            <li class="theme-interest-tab" data-id="rubrics">Rubriques</li>            
-        </ul>         
+            <li class="theme-interest-tab <?php echo $active == '1' ? 'active' : '' ?>"><a href="<?php echo osc_static_page_url(); ?>"> Themes</a></li>
+            <li class="theme-interest-tab <?php echo $active == '2' ? 'active' : '' ?>"><a href="<?php echo osc_static_page_url() . '&type=interest'; ?>">Rubriques</a></li>            
+        </ul> 
     </div>
-    <form action="" method="post"  class="user_rubric_form">       
-        <div class="container">
-            <div class="row">                 
-                <div class="col-md-12 col-sm-12">
-                    <div class="row">                         
-                        <?php foreach ($themes as $k => $theme): ?>
-                            <div class="col-md-2 col-sm-2 margin-bottom-20">
-                                <div class="category_box" data-id="<?php echo $theme['pk_i_id'] ?>">
-                                    <div class="category_image">
-                                        <?php
-                                        if ($theme['bs_image_name']) :
-                                            $img_path = UPLOAD_PATH . $theme['bs_image_name'];
-                                        else:
-                                            $img_path = osc_current_web_theme_url() . 'images/no-photo.jpg';
-                                        endif;
-                                        ?>
-                                        <img src="<?php echo $img_path; ?>" class="img img-responsive cat-image"/>   
-                                        <div class="add_box">
-                                            <span class="add_icon"></span>
+    <?php if (isset($_GET['type']) && $_GET['type'] == 'interest'): ?>
+        <form action="" method="post"  class="user_theme_form">       
+            <div class="container">
+                <div class="row">                 
+                    <div class="col-md-12 col-sm-12">
+                        <div class="row">                         
+                            <?php foreach ($interest as $k => $i): ?>
+                                <div class="col-md-2 col-sm-2 margin-bottom-20">
+                                    <div title="<?php echo $i['s_name']; ?>" class="category_box <?php echo in_array($i['fk_i_category_id'], $selected_rubric_arr) ? 'selected' : '' ?>">
+                                        <div class="category_image">
+                                            <?php
+                                            if ($i['bs_image_name']) :
+                                                $img_path = UPLOAD_PATH . $i['bs_image_name'];
+                                            else:
+                                                $img_path = osc_current_web_theme_url() . 'images/no-photo.jpg';
+                                            endif;
+                                            ?>
+                                            <img src="<?php echo $img_path; ?>" class="img img-responsive cat-image"/>   
+                                            <div class="add_box">
+                                                <span class="add_icon"></span>
+                                            </div>
+                                            <div class="overlay"></div>
+                                            <input type="checkbox" name="cat_id[]" value="<?php echo $i['fk_i_category_id'] ?>" class="cat_checkbox" <?php echo in_array($i['fk_i_category_id'], $selected_rubric_arr) ? 'checked="checked"' : '' ?> style="display: none">
                                         </div>
-                                        <div class="overlay"></div>
-                                        <input type="checkbox" name="cat_id[]" value="<?php echo $theme['pk_i_id'] ?>" class="cat_checkbox" style="display: none">
-                                    </div>
-                                    <div class="category_title">
-                                        <?php echo osc_highlight($theme['s_name'], 13); ?>
+                                        <div class="category_title">
+                                            <?php echo osc_highlight($i['s_name'], 10); ?>                                            
+                                        </div>
                                     </div>
                                 </div>
-                            </div>
-                            <?php
-                        endforeach;
-                        ?>
+                                <?php
+                            endforeach;
+                            ?>
+                        </div>
                     </div>
-                </div>
+                </div>            
+            </div>
+        </form>
+    <?php else: ?>    
+        <form action="<?php echo osc_static_page_url(); ?>" method="post"  class="user_theme_form">       
+            <div class="container">
+                <div class="row">                 
+                    <div class="col-md-12 col-sm-12">
+                        <div class="row">                         
+                            <?php foreach ($themes as $k => $theme): ?>
+                                <div class="col-md-2 col-sm-2 margin-bottom-20">
+                                    <div title="<?php echo $theme['s_name']; ?>" class="category_box <?php echo in_array($theme['pk_i_id'], $selected_themes_arr) ? 'selected' : '' ?>" data-id="<?php echo $theme['pk_i_id'] ?>">
+                                        <div class="category_image">
+                                            <?php
+                                            if ($theme['bs_image_name']) :
+                                                $img_path = UPLOAD_PATH . $theme['bs_image_name'];
+                                            else:
+                                                $img_path = osc_current_web_theme_url() . 'images/no-photo.jpg';
+                                            endif;
+                                            ?>
+                                            <img src="<?php echo $img_path; ?>" class="img img-responsive cat-image"/>   
+                                            <div class="add_box">
+                                                <span class="add_icon"></span>
+                                            </div>
+                                            <div class="overlay"></div>
+                                            <input type="checkbox" name="cat_id[]" value="<?php echo $theme['pk_i_id'] ?>" class="cat_checkbox" <?php echo in_array($theme['pk_i_id'], $selected_themes_arr) ? 'checked="checked"' : '' ?> style="display: none">
+                                        </div>
+                                        <div class="category_title">
+                                            <?php echo osc_highlight($theme['s_name'], 13); ?>
+                                        </div>
+                                    </div>
+                                </div>
+                                <?php
+                            endforeach;
+                            ?>
+                        </div>
+                    </div>
+                </div>            
             </div>            
-        </div>
-    </form>
+        </form>
+    <?php endif; ?>
 </div>
 <?php
 
@@ -122,20 +181,14 @@ function new_footer() {
     ?>
     <script>
         $(document).ready(function () {
-    //            $('.category_box').click(function () {
-    //                $(this).toggleClass('selected');
-    //                var checkbox = $(this).find('.cat_checkbox');
-    //                checkbox.attr("checked", !checkbox.attr("checked"));
-    //            });
-    //            $('.add_rubric').click(function (e) {
-    //                var checked_cat = $('.category_box.selected');
-    //                if (checked_cat.length >= 4) {
-    //                    $('.user_rubric_form').submit();
-    //                } else {
-    //                    alert('Please select at least four rubrics');
-    //                    e.preventDefault();
-    //                }
-    //            });
+            $('.category_box').click(function () {
+                $(this).toggleClass('selected');
+                var checkbox = $(this).find('.cat_checkbox');
+                checkbox.attr("checked", !checkbox.attr("checked"));
+            });
+            $('.update_theme').click(function (e) {
+                $('.user_theme_form').submit();
+            });
         });
     </script>
     <?php
---
+++ oc-content/themes/flatter/soundpass.php
@@ -0,0 +1,29 @@
+
+<?php
+require '../../../oc-load.php';
+require 'functions.php';
+?>
+<?php osc_current_web_theme_path('header.php'); ?>
+
+<!-- profile cover -->
+
+<div class="soundpass-page">
+    <div class="cover-img">
+        <div class="container">
+            <div class="min-height-300"></div>                
+            <div class="col-md-8">                
+                <h1 class="font-color-white bold-600 font-65px">Sound Pass</h1>                
+                <h2 class="font-color-white bold-600">Lorem Ipsum is simply dummy text</h2>                
+            </div>
+        </div>
+        <div class="contant bg-white col-md-12">
+            <div class="col-md-7 col-md-offset-1 text-left padding-top-4per ">
+                <h4 class="bold-600 margin-0 text-gray-dark">Lorem Ipsum is simply dummy textLorem Ipsum is simply dummy textLorem Ipsum is simply dummy textLorem Ipsum is simply dummy textLorem Ipsum is simply dummy text</h4>
+            </div>            
+            <div class="col-md-4 padding-top-4per text-center">
+                <button type="submit" class="btn btn-lg button-orng" data-toggle="modal" data-target="#payment">Activer sound Pass</button>
+            </div>                
+
+        </div>
+        <!-- end profil cover -->
+        <?php osc_current_web_theme_path('footer.php'); ?>
